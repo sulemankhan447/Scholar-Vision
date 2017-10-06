be5ffeb841b3837d4e55c4a68995424965e0791e
sobiya <sobiyasardar@gmail.com>
2017-10-07 00:17:39 +0530

    autograding script without plaguarism

--- python/autograder.py
+++ python/autograder.py
@@ -12,11 +12,10 @@ db = "EADB"
 
 m = 0
 
-
 db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
 cur = db.cursor()
 
-# -----------------------Get data from tables---------------------------
+# -----------------------Get data from Database---------------------------
 
 cur.execute("select rollno, name from student where rollno = '1' ")
 #cur.execute("select tos, code, output from code where rollno = ? ")
@@ -27,15 +26,9 @@ cur.execute("select tos, code, output from code where rollno = '1' ")
 tos, code, output = cur.fetchone()
 
 
-#print(output)
-#print(type(output))
-#print(type(tos))
-#print(code)
 cur.execute("select dos, writeup from exp_detail where s_name = 'OS' and e_id = 1")
 #cur.execute("select dos, writeup from exp_detail where s_name = ? and e_id = ?")
-
 dos, writeup = cur.fetchone()
-#print(dos)
 
 
 # -----------------------Grading----------------------------------------
@@ -58,35 +51,6 @@ cur.execute("update code set marks=%s where rollno='1' " % (m))
 #cur.execute("update code set marks=%s where rollno=? " % (m))
 
 db.commit()
-#db.close()
-
-# --------------Create Final printable File and insert into DB-------------------------
-
-# str(code) and str(output) is used here because it may psble code or output is empty
-rollno = "Roll No: " + rollno
-name = "Name: " + name
-code = "Code:\n" + str(code)
-output = "Output:\n" + str(output)
-marks = "Marks: " + str(m)
-
-final = rollno + "\n" + name + "\n\n\n" +code + "\n\n\n" + output + "\n\n\n" + marks
-print(final)
-
-
-#f = open("/var/www/html/final.txt","w")
-#f.write(final)
-#f.close()
-cur.execute("LOAD DATA LOCAL INFILE '/var/www/html/final.txt' INTO TABLE final")
-#cur.execute("insert into final (rollno, file)  values('1', 'final.txt');")
-cur.execute("select file from final where rollno = '1'")
-f1 = cur.fetchone()
-
-#open(f1,'r')           
-#f3 = f1.read()
-#print(f3)
-#f2.close()
-
-
-db.commit()
 db.close()
+
 
